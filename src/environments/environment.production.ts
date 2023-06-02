e0006af134bba8da68802031d5f44bfc60c91b9c
Nikolas Thitu <nikolas@hauz.io>
2023-06-02 13:33:22 -0400

    update

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 import { Environment } from '@trusona/webauthn'
 
 export const environment = {
-  production: true,
-  sdkId: process.env['SDK_ID'],
+  production: true, 
+  sdkId: '1fa48a3f-e20f-4062-a99c-08eecc300ec7',
   sdkEnvironment: Environment.PRODUCTION
 }
