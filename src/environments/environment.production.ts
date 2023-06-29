06e33887cc096b33dd72bec380fdf07a1d3a4f3c
david <foster@hauz.io>
2023-06-29 16:23:35 -0600

    added browser check ui and updated to new sdk

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,4 @@
-import { Environment } from '@trusona/webauthn'
-
 export const environment = {
   production: true,
-  sdkId: '1fa48a3f-e20f-4062-a99c-08eecc300ec7',
-  sdkEnvironment: Environment.PRODUCTION
+  sdkOrigin: 'authcloud-sdk-demo.trusona.com'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
-import { Environment } from '@trusona/webauthn'
-
 export const environment = {
   production: false,
-  sdkId: '9d6d5a49-a35c-49aa-aadb-1cccfa7294a3',
-  sdkEnvironment: Environment.STAGING
+  sdkOrigin: 'localhost'
 }
